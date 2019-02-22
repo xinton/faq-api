4a54e7c678c158e02d12715d76da19d4267575ce
xinton <washingtonbruno1@gmail.com>
2019-02-22 14:51:12 -0300

    Adicionando model de topicos

--- faq/models.py
+++ faq/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Topic(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    tittle = models.CharField(max_length=100, blank=False)
+    text = models.TextField()
